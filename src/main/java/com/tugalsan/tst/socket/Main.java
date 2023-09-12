d0cc24d0f3b25a137fdf5ee7881162c021c2f43e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-12 09:14:06 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/socket/Main.java
@@ -0,0 +1,39 @@
+package com.tugalsan.tst.socket;
+
+import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.socket.server.TS_SocketClient;
+import com.tugalsan.api.socket.server.TS_SocketServer;
+import com.tugalsan.api.socket.server.TS_SocketUtils;
+import com.tugalsan.api.thread.server.TS_ThreadWait;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final TS_Log d = TS_Log.of(Main.class);
+
+    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.socket
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.socket-1.0-SNAPSHOT-jar-with-dependencies.jar
+    //java -jar target/com.tugalsan.tst.socket-1.0-SNAPSHOT-jar-with-dependencies.jar
+    public static void main(String... s) {
+        var killTrigger = TS_ThreadSyncTrigger.of();
+        var port = 8282;
+        if (!TS_SocketUtils.available(8282)) {
+            d.ce("main", "ERROR: Port in use already!", port);
+            return;
+        }
+        var server = TS_SocketServer.of(killTrigger, port, forEachReceivedLine -> {
+            return forEachReceivedLine.toUpperCase();
+        }).start();
+        var clinet = TS_SocketClient.of(killTrigger, port, forEachReceivedLine -> {
+            d.cr("client", "server sent", forEachReceivedLine);
+        }).start();
+
+        IntStream.range(0, 10).forEachOrdered(i -> {
+            TS_ThreadWait.milliseconds100();
+            d.cr("client", "addToQueue", "ĞÜğüŞİşiÖçöçIıı " + i);
+        });
+        TS_ThreadWait.seconds(null, killTrigger, 15);
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.tugalsan.tst.socket {
+    requires com.tugalsan.api.socket;
+    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.log;
+}
