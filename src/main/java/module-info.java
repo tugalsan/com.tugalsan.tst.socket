d5b68c9b7536bc89736da6c0b62d0d479d6a2490
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:04:12 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.tugalsan.tst.socket {
     requires com.tugalsan.api.socket;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.log;
